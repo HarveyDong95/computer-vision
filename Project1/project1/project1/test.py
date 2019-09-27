66e82add7a660c2412960de9abc22f8d37281f11
harveydyy <harveydongzj@gmail.com>
2019-09-26 22:55:02 -0400

    project1

---
+++ Project1/project1/project1/test.py
@@ -0,0 +1,23 @@
+i = [[1,2,3,3],[4,5,6,1],[7,1,2,2]]
+j = [1,2]
+
+print(i[1][2])
+
+# for s in i[:]:
+#     for x in s[:]:
+#         print(x)
+# i[2][2]
+
+# m = []
+
+# m =  [[i[0][0],i[0][1]],[i[1][0],i[1][1]]]
+# print(m)
+
+
+# result = []
+# for m in range(0,2):
+#     for n in range(0,3):
+#         temp = i[m][:n]
+#     result.append(temp)
+# print(result)
+# print([m[:1] for m in i[:2]])
